25e30ebcbda38f3773722b9ef4dbc279ace1be85
sejongjeong <wjdtpwhd@gmail.com>
2022-01-22 23:18:25 +0900

    solving: Create linked list class

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -1,8 +1,27 @@
+from typing import *
+
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Single:
+    def __init__(self, lst):
+        self.h = None
+        pt = None
+        for i, v in enumerate(lst):
+            if i == 0:
+                self.h = ListNode(val=v)
+                pt = self.h
+            else:
+                tmp = ListNode(v)
+                pt.next = tmp
+                pt = pt.next
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        
