7d72b3d9f6f08ef9a227b73ca856ffca24f675e7
sejongjeong <wjdtpwhd@gmail.com>
2022-01-25 23:58:40 +0900

    solving: Add single linked list

--- 206_Reverse_Linked_List.py
+++ 206_Reverse_Linked_List.py
@@ -3,6 +3,23 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+class Single:
+    def __init__(self, lst):
+        self.h = None
+        pt = None
+        for i, v in enumerate(lst):
+            if i == 0:
+                self.h = ListNode(val=v)
+                pt = self.h
+            else:
+                tmp = ListNode(v)
+                pt.next = tmp
+                pt = pt.next
+
+
+from typing import *
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
