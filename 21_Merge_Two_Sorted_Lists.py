8c0fa8ee063586a6874474b33bcc71580d83646e
sejongjeong <wjdtpwhd@gmail.com>
2022-01-26 23:58:50 +0900

    add: 21 another solution

--- 21_Merge_Two_Sorted_Lists.py
+++ 21_Merge_Two_Sorted_Lists.py
@@ -2,10 +2,12 @@ from typing import *
 
 # Definition for singly-linked list.
 
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 
 class Single:
     def __init__(self, lst):
@@ -21,33 +23,44 @@ class Single:
                 pt = pt.next
 
 # Test Solution
+# class Solution:
+#     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+#         head1, head2 = list1, list2
+#         new = None
+#         if head1 is None and head2 is None:
+#             return None
+#         if head2 is None or (head1 and head2 and head1.val < head2.val):
+#             new = head1
+#             head1 = head1.next
+#         elif head1 is None or (head1 and head2 and head1.val >= head2.val):
+#             new = head2
+#             head2 = head2.next
+#         ptr = new
+#         while head1 or head2:
+#             if head2 is None or(head1 and head2 and head1.val < head2.val):
+#                 ptr.next = head1
+#                 ptr = ptr.next
+#                 head1 = head1.next
+#             elif head1 is None or (head1 and head2 and head1.val >= head2.val):
+#                 ptr.next = head2
+#                 ptr = ptr.next
+#                 head2 = head2.next
+#         ptr.next = None
+#         return new
+# Recursion
+
+
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        head1, head2 = list1, list2
-        new = None
-        if head1 is None and head2 is None:
-            return None
-        if head2 is None or (head1 and head2 and head1.val < head2.val):
-            new = head1
-            head1 = head1.next
-        elif head1 is None or (head1 and head2 and head1.val >= head2.val):
-            new = head2
-            head2 = head2.next
-        ptr = new
-        while head1 or head2:
-            if head2 is None or(head1 and head2 and head1.val < head2.val):
-                ptr.next = head1
-                ptr = ptr.next
-                head1 = head1.next
-            elif head1 is None or (head1 and head2 and head1.val >= head2.val):
-                ptr.next = head2
-                ptr = ptr.next
-                head2 = head2.next
-        ptr.next = None
-        return new
+        if (not list1) or (list2 and list1.val > list2.val):
+            list1, list2 = list2, list1
+        if list1:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+        return list1
+
 
 test = Solution()
-t_ptr = test.mergeTwoLists(None, Single([1,3,4,6,11]).h)
+t_ptr = test.mergeTwoLists(None, Single([1, 3, 4, 6, 11]).h)
 while t_ptr:
     print(t_ptr.val)
     t_ptr = t_ptr.next
