88b8ae176616e82fd1c1420d1dd6678baa0ba162
sejongjeong <wjdtpwhd@gmail.com>
2022-01-29 14:47:03 +0900

    solved: 206

--- 206_Reverse_Linked_List.py
+++ 206_Reverse_Linked_List.py
@@ -4,6 +4,7 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Single:
     def __init__(self, lst):
         self.h = None
@@ -20,6 +21,29 @@ class Single:
 
 from typing import *
 
+# Solution using loop
+# class Solution:
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         node, prev = head, None
+#         while node:
+#             next, node.next = node.next, prev
+#             node, prev = next, node
+#         return prev
+
+# Solution using recursion
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
+        def rev(node: ListNode, prev: ListNode = None):
+            if not node:
+                return prev
+            next, node.next = node.next, prev
+            return rev(next, node)
+
+        return rev(head)
+
+
+test = Solution()
+t_ptr = test.reverseList(Single([1, 2, 5, 6, 8, 9, 11]).h)
+while t_ptr:
+    print(t_ptr.val)
+    t_ptr = t_ptr.next
